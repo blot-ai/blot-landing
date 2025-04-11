a05348096f099634801b910675ba0321c4d0d3f7
lakshith <lakshith.k.nishshanke@gmail.com>
2025-04-11 12:50:03 +0530

    unpotimize images

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,7 @@ const nextConfig: NextConfig = {
     /* config options here */
     output: "export",
     images: {
+        unoptimized: true,
         domains: ["images.unsplash.com", "assets.aceternity.com"],
     },
     basePath: "/blot-landing",
